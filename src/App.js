dafb9a7761c0d4b940ad679be19071ca739110cc
alexanderwilly <alexanderwillyj@gmail.com>
2024-09-27 11:19:07 +0800

    added nav bar and contents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import './App.css';
+import Navbar from './props/navbar';
+// import React, { useEffect, useState } from 'react';
 
-function App() {
+
+const App = () => {
+  // const [activeSection, setActiveSection] = useState('home');
+
+  // useEffect(() => {
+  //   const sections = document.querySelectorAll('section');
+
+  //   const handleScroll = () => {
+  //     let scrollPosition = window.scrollY;
+
+  //     sections.forEach((section) => {
+  //       const offsetTop = section.offsetTop - 150;
+  //       const sectionHeight = section.offsetHeight;
+  //       const sectionId = section.getAttribute('id');
+
+  //       if (scrollPosition >= offsetTop && scrollPosition < offsetTop + sectionHeight) {
+  //         setActiveSection(sectionId);
+  //       }
+  //     });
+  //   };
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const navLinks = document.querySelectorAll('nav a');
+
+  //   navLinks.forEach((link) => {
+  //     link.classList.remove('active');
+  //     if (link.getAttribute('href').includes(activeSection)) {
+  //       link.classList.add('active');
+  //     }
+  //   });
+  // }, [activeSection]);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <body>
+      <Navbar />
+      <section id = "home">HOME</section>
+      <section id = "about">ABOUT</section>
+      <section id = "edu">EDUCATION</section>
+      <section id = "certs">CERTIFICATIONS</section>
+      <section id = "projects">PROJECTS</section>
+      <section id = "skills">SKILLS</section>
+      <section id = "contact">CONTACT</section>
+      
+      
+    </body>
   );
 }
 
---
+++ src/props/navbar.js
@@ -0,0 +1,41 @@
+import './navbar.css';
+import logo from './img/logo.png';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import {useRef} from "react";
+// https://www.youtube.com/watch?v=amf18mxNX18
+export default function Navbar() {
+    const navRef = useRef();
+
+    const showNavbar = ()=>{
+        navRef.current.classList.toggle('responsive_nav');
+    }
+
+    return <nav className = "nav bg-slate-950 text-white fixed flex top-0 left-0 w-full px-16 py-6 justify-between items-center z-100">
+        <img src = {logo} alt = "logo" className = "w-100 h-12" />
+
+        <div className = "flex content-center">
+            <ul ref = {navRef}>
+                {/* <li><a className='active' href = "#home">HOME</a></li> */}
+                <li><a href = "#home">HOME</a></li>
+                <li><a href = "#about">ABOUT</a></li>
+                <li><a href = "#edu">EDUCATION</a></li>
+                <li><a href = "#certs">CERTIFICATIONS</a></li>
+                <li><a href = "#projects">PROJECTS</a></li>
+                <li><a href = "#skills">SKILLS</a></li>
+                <li><a href = "#contact">CONTACT</a></li>
+                <button className = 'nav-btn nav-close-btn' onClick = {showNavbar}>
+                    <FaTimes />
+                </button>
+                
+            </ul>
+            <button className = 'nav-btn' onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </div>
+
+        
+        
+
+        
+    </nav>
+}
